5e7ecc16a23e4e2cd3facf0a9388c94e768045dd
Chad Tarpey <chadtarpey@gmail.com>
2020-11-09 20:35:17 -0600

    Started to work on the main.test html

---
+++ public/assets/js/index.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3V0b2Z0dW5lMjY2IiwiYSI6ImNraGIxZGh4aDA4OXYyd3FwdTY2azRwNngifQ.w_qP1F_2EMAK3xAwx_enKA';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/outoftune266/ckhb1hfdo03im19pd13h9rez2', // style URL
+    center: [-86.7, 36.1], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+
+});
