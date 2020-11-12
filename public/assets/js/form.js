63d8b16c7a1d22859c101af2c5f200336a63daee
Zak Monnet <zakmonnet@gmail.com>
2020-11-12 10:31:29 -0600

    fixed post request in form.js

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -8,7 +8,7 @@
 // }
 
 function getLocation() {
-    let city = $("#coord").val();
+    let address = $("#coord").val();
     let name = $("#name").val();
     let cuisine = $("#cuisine").val();
     let cost = $("#cost").val();
@@ -17,7 +17,7 @@ function getLocation() {
     let family = $("#family").val();
     let mustTry = $("#try").val();
     console.log(family);
-    let queryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + city + "&key=d0ec5acfd95d41b2b3da1850a8ae6d1a";
+    let queryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + address + "&key=d0ec5acfd95d41b2b3da1850a8ae6d1a";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -25,7 +25,7 @@ function getLocation() {
         console.log(response);
         lat = response.results[0].geometry.lat;
         lng = response.results[0].geometry.lng;
-        newLocation(name, cuisine, location, cost, url, rating, lat, lng, family, mustTry)
+        newLocation(name, cuisine, address, cost, url, rating, lat, lng, family, mustTry)
     });
 };
 
@@ -36,48 +36,40 @@ $(".submit").on("click", function (e) {
 });
 
 
-function newLocation(name, cuisine, location, cost, url, rating, lat, lng, family, mustTry) {
+function newLocation(name, cuisine, address, cost, url, rating, lat, lng, family, mustTry) {
     //console.log(name);
     // let data = {
-    //     restaurantName: name,
-    //     cuisine: cuisine,
-    //     location: location,
-    //     cost: cost,
-    //     website: url,
-    //     rating: rating,
-    //     latitude: lat,
-    //     longitude: lng,
-    //     familyFriendly: family,
-    //     mustTry: mustTry
+    // restaurantName: name,
+    // cuisine: cuisine,
+    // location: location,
+    // cost: cost,
+    // website: url,
+    // rating: rating,
+    // latitude: lat,
+    // longitude: lng,
+    // familyFriendly: family,
+    // mustTry: mustTry
     // };
-    // $.ajax({
-    //     url: "/api/food",
-    //     method: "POST",
-    //     data: {
-    //         restaurantName: name
-    //     },
-    //     dataType: "json"
-    // }).then(() => {
-    //         alert("hi");
-    //     })
-    $.post("/api/food",
-        {
+    $.ajax({
+        url: "/api/food",
+        method: "POST",
+        data: {
             restaurantName: name,
             cuisine: cuisine,
-            location: location,
+            location: address,
             cost: cost,
             website: url,
             rating: rating,
             latitude: lat,
             longitude: lng,
             familyFriendly: family,
-            mustTry: mustTry
+            mustTry: mustTry,
         },
-        function (data, status) {
-            console.log(data);
-            console.log(status);
-        });
-};
+        dataType: "json"
+    }).then(() => {
+        alert("hi");
+    })
+}
 // ).done(function (data) {
 //         alert("Thanks for entering a restaurant!!!!!!!")
 //         console.log(data);
