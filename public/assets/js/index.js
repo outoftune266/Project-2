5d3c1cae768dddbaeef8ca4ea7084e6c4dc20614
Chad Tarpey <chadtarpey@gmail.com>
2020-11-11 20:31:52 -0600

    Added form.js and form.css

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,7 +7,7 @@ const logo = document.querySelector(".logo");
 //const  = document.querySelector(".hero");
 const headline = document.querySelector(".headline");
 
-const tl = gsap.timeline({defaults: {ease: "power1.out"}});
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 //Map Instantiation
 mapboxgl.accessToken = 'pk.eyJ1Ijoib3V0b2Z0dW5lMjY2IiwiYSI6ImNraGF4NnhwZDBrZjMzMms0c2xwejYydmEifQ.CsvsPCXbKiZI9P_psvhAgw';
@@ -122,9 +122,9 @@ map.on('load', function () {
 
 });
 
-    setTimeout(function(){
-        $("#map").attr("style", "z-index: 99")
-    },4000)
+setTimeout(function () {
+    $("#map").attr("style", "z-index: 99")
+}, 4000)
 
 //Click Listener
 $(".btn").on("click", () => {
@@ -132,10 +132,10 @@ $(".btn").on("click", () => {
     console.log(bounds);
 })
 
-tl.to(".text", { y:"0%", duration: 1, stagger: 0.55});
-tl.to(".slider", {y: "-100%", duration:1.5, delay: 0.5 });
-tl.to(".intro", {y: "-100%", duration: 1}, "-=1");
-tl.fromTo("nav", {opacity: 0}, {opacity:1, duration: 1})
-tl.fromTo(".big-text", {opacity: 0}, {opacity:1, duration: 1}, "-=1")
-tl.fromTo(".toggle", {opacity: 0}, {opacity:1, duration: 1}, "-=1")
-tl.fromTo("footer", {opacity: 0}, {opacity:1, duration: 1}, "-=1")
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.55 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 })
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1")
+tl.fromTo(".toggle", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1")
+tl.fromTo("footer", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1")
